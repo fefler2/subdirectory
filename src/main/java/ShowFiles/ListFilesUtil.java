baecae0c1102a57a89eea60078c71f8ca81d0da7
giciarz412 <tomaszfefler1990@gmail.com>
2019-04-27 09:13:03 +0200

    subdirectory - sukces! dziala2

--- src/main/java/ShowFiles/ListFilesUtil.java
+++ src/main/java/ShowFiles/ListFilesUtil.java
@@ -1,81 +1,88 @@
-package ShowFiles;
-
-
-import java.io.File;
-
-public class ListFilesUtil{
-
-
-    public void listFilesAndFolders(String directoryName){
-        File directory = new File(directoryName);
-        //get all the files from a directory
-        File[] fList = directory.listFiles();
-        for (File file : fList){
-            System.out.println(file.getName());
-        }
-    }
-
-    public void listFiles(String directoryName){
-        File directory = new File(directoryName);
-        //get all the files from a directory
-        File[] fList = directory.listFiles();
-        assert fList != null;
-        for (File file : fList){
-            if (file.isFile()){
-                System.out.println(file.getName());
-            }
-        }
-    }
-    /**
-     * List all the folder under a directory
-     * @param directoryName to be listed
-     */
-    public void listFolders(String directoryName){
-        File directory = new File(directoryName);
-        //get all the files from a directory
-        File[] fList = directory.listFiles();
-        assert fList != null;
-        for (File file : fList){
-            if (file.isDirectory()){
-                System.out.println(file.getName());
-            }
-        }
-    }
-
-    /**
-     * List all files from a directory and its subdirectories
-     * @param directoryName to be listed
-     */
-    public void listFilesAndFilesSubDirectories(String directoryName){
-        File directory = new File(directoryName);
-        //get all the files from a directory
-        File[] fList = directory.listFiles();
-        assert fList != null;
-        for (File file : fList){
-//            if (file.isFile())
-
-
-            if (file.isFile()){
-                System.out.println(file.getAbsolutePath());
-                // dotad dziala
-
-            }
-
-// else if (file.isDirectory()){
+//package ShowFiles;
+//
+//
+//import java.io.File;
+//
+//public class ListFilesUtil {
+//
+//
+//    public void listFilesAndFolders(String directoryName) {
+//        File directory = new File(directoryName);
+//        //get all the files from a directory
+//        File[] fList = directory.listFiles();
+//        for (File file : fList) {
+//            System.out.println(file.getName());
+//        }
+//    }
+//
+//    public void listFiles(String directoryName) {
+//        File directory = new File(directoryName);
+//        //get all the files from a directory
+//        File[] fList = directory.listFiles();
+//        assert fList != null;
+//        for (File file : fList) {
+//            if (file.isFile()) {
+//                System.out.println(file.getName());
+//            }
+//        }
+//    }
+//
+//    /**
+//     * List all the folder under a directory
+//     *
+//     * @param directoryName to be listed
+//     */
+//    public void listFolders(String directoryName) {
+//        File directory = new File(directoryName);
+//        //get all the files from a directory
+//        File[] fList = directory.listFiles();
+//        assert fList != null;
+//        for (File file : fList) {
+//            if (file.isDirectory()) {
+//                System.out.println(file.getName());
+//            }
+//        }
+//    }
+//
+//    /**
+//     * List all files from a directory and its subdirectories
+//     *
+//     * @param directoryName to be listed
+//     */
+//    public void listFilesAndFilesSubDirectories(String directoryName) {
+//        File directory = new File(directoryName);
+//        //get all the files from a directory
+//        File[] fList = directory.listFiles();
+//        assert fList != null;
+//        for (File file : fList) {
+////            if (file.isFile())
+//
+//
+//
+//            if (file.isFile()) {
+//                System.out.println(file.getAbsolutePath());
+//                // dotad dziala
+//
+//            }
+//
+//            //
+//
+//            else if (  file.isDirectory() && file.isDirectory()) {
 //                listFilesAndFilesSubDirectories(file.getAbsolutePath());
 //            }
-
-        }
-    }
-    public static void main (String[] args){
-        ListFilesUtil listFilesUtil = new ListFilesUtil();
-        final String directory ="C:\\";
-        //Windows directory example
-        final String directoryWindows ="C://test";
-        String directory2 = "";
-        System.out.println("Foldery:");
-//        listFilesUtil.listFiles(directory);
-        System.out.println();
-        listFilesUtil.listFilesAndFilesSubDirectories(directory);
-    }
-}
+//
+//        }
+//    }
+//
+//    public static void main(String[] args) {
+//        ListFilesUtil listFilesUtil = new ListFilesUtil();
+//        final String directory = "C:\\";
+//        //Windows directory example
+//        final String directoryWindows = "C://test";
+//        String directory2 = "";
+//        System.out.println("Foldery:");
+////        listFilesUtil.listFiles(directory);
+//        System.out.println();
+//        listFilesUtil.listFilesAndFilesSubDirectories(directory);
+//    }
+//}
---
+++ src/main/java/ShowFiles/ListFilesUtil2.java
@@ -0,0 +1,95 @@
+package ShowFiles;
+
+
+import java.io.File;
+
+public class ListFilesUtil2 {
+
+
+    public void listFilesAndFolders(String directoryName) {
+        File directory = new File(directoryName);
+        //get all the files from a directory
+        File[] fList = directory.listFiles();
+        for (File file : fList) {
+            System.out.println(file.getName());
+        }
+    }
+
+    public void listFiles(String directoryName) {
+        File directory = new File(directoryName);
+        //get all the files from a directory
+        File[] fList = directory.listFiles();
+        assert fList != null;
+        for (File file : fList) {
+            if (file.isFile()) {
+                System.out.println(file.getName());
+            }
+        }
+    }
+
+    /**
+     * List all the folder under a directory
+     *
+     * @param directoryName to be listed
+     */
+    public void listFolders(String directoryName) {
+        File directory = new File(directoryName);
+        //get all the files from a directory
+        File[] fList = directory.listFiles();
+        assert fList != null;
+        for (File file : fList) {
+            if (file.isDirectory()) {
+                System.out.println(file.getName());
+            }
+        }
+    }
+
+    /**
+     * List all files from a directory and its subdirectories
+     *
+     * @param directoryName to be listed
+     */
+    public void listFilesAndFilesSubDirectories(String directoryName) {
+        File directory = new File(directoryName);
+        //get all the files from a directory
+        File[] fList = directory.listFiles();
+        assert fList != null;
+        for (File file : fList) {
+//            if (file.isFile())
+
+
+            try {
+                if (file.isFile()) {
+                    System.out.println(file.getAbsolutePath());
+                    // dotad dziala
+
+                }
+
+                //
+
+
+                else if (file.isDirectory() && file.isDirectory()) {
+                    listFilesAndFilesSubDirectories(file.getAbsolutePath());
+                }
+
+
+            }
+            catch (NullPointerException e){
+//                continue;
+            }
+
+        }
+    }
+
+    public static void main(String[] args) {
+        ListFilesUtil2 listFilesUtil = new ListFilesUtil2();
+        final String directory = "C:\\";
+        //Windows directory example
+        final String directoryWindows = "C://test";
+        String directory2 = "";
+        System.out.println("Foldery:");
+//        listFilesUtil.listFiles(directory);
+        System.out.println();
+        listFilesUtil.listFilesAndFilesSubDirectories(directory);
+    }
+}
